092c7fc6638fe217b866e434522c73ef148c155c
Eya <cos19waz@hhu.de>
2025-03-16 20:46:24 +0100

    TestForm

---
+++ src/main/java/org/example/exambyte/controllers/organisator/OrganisatorController.java
@@ -0,0 +1,8 @@
+package org.example.exambyte.controllers.organisator;
+
+public class OrganisatorController {
+
+  
+
+
+}
---
+++ src/main/java/org/example/exambyte/controllers/organisator/TestForm.java
@@ -0,0 +1,7 @@
+package org.example.exambyte.controllers.organisator;
+
+public class TestForm {
+private String fragestellung;
+private int maxPunkte;
+private int ;
+}
